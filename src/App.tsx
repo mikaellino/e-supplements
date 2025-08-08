f7bb889168814d77a85c18598699e115b102adec
mikaellino <mikael.lino123@gmail.com>
2025-08-08 12:24:46 -0300

    refactor: ajusta estrutura principal da aplicação e roteamento

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,29 @@
 import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home'
 import Sidebar from './components/Sidebar'
+import Footer from './components/Footer';
+import Whey from './pages/Whey';
+import Creatina from './pages/Creatina';
+import PreTreino from './pages/PreTreino';
+import Vitaminas from './pages/Vitaminas';
+
 import './App.css'
 
 function App() {
   return (
     <div className="flex h-screen bg-gray-100">
-      {/* Sidebar */}
       <Sidebar />
-      
-      {/* Main Content */}
       <div className="flex-1 overflow-auto">
-        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/whey" element={<Whey />} />
+          <Route path="/creatina" element={<Creatina />} />
+          <Route path="/pre-treino" element={<PreTreino />} />
+          <Route path="/vitaminas" element={<Vitaminas />} />
+        </Routes>
       </div>
+      <Footer />
     </div>
   )
 }
