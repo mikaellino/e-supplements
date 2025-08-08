e0bc31d0e9d446a147e2ad1f72dfc847d940d932
mikaellino <mikael.lino123@gmail.com>
2025-08-08 12:24:47 -0300

    feat: atualiza página de Whey

--- src/pages/Whey.tsx
+++ src/pages/Whey.tsx
@@ -1,9 +1,37 @@
+// src/pages/Whey.tsx
+
 import React from 'react';
+import ProductList from '../components/ProductList';
+import wheyDefaultImage from '../assets/wheypadrao.webp';
+
+const wheyProducts = [
+  {
+    id: 1,
+    name: 'Whey Protein Concentrado',
+    price: 129.90,
+    image: wheyDefaultImage,
+    quantity: 50,
+  },
+  {
+    id: 2,
+    name: 'Whey Protein Isolado',
+    price: 189.90,
+    image: wheyDefaultImage,
+    quantity: 30,
+  },
+  {
+    id: 3,
+    name: 'Blend de Proteínas',
+    price: 109.90,
+    image: wheyDefaultImage,
+    quantity: 75,
+  },
+];
 
 const Whey: React.FC = () => {
   return (
     <div>
-      <h1>Página em desenvolvimento</h1>
+      <ProductList products={wheyProducts} title="Whey Protein" />
     </div>
   );
 };
